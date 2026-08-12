39897e12ac764d21e80d1ce30ff9025350bbe4c1
elephant-tech-labs <official.elephanttechlabs@gmail.com>
2026-08-12 18:11:38 +0530

    Add Zoho contact messaging widget route

---
+++ src/app/widget/contact/page.tsx
@@ -0,0 +1,7 @@
+import ContactMessagingWidget from "./ContactMessagingWidget";
+
+export const dynamic = "force-dynamic";
+
+export default function ContactWidgetPage() {
+  return <ContactMessagingWidget />;
+}
