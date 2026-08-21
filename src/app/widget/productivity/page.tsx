c8f1b072515058d25516f1224a2449780c059cc1
elephant-tech-labs <official.elephanttechlabs@gmail.com>
2026-08-21 16:05:08 +0530

    Add MCC SMS productivity workspace page

---
+++ src/app/widget/productivity/page.tsx
@@ -0,0 +1,7 @@
+import SmsProductivityWorkspace from "./SmsProductivityWorkspace";
+
+export const dynamic = "force-dynamic";
+
+export default function SmsProductivityPage() {
+  return <SmsProductivityWorkspace />;
+}
